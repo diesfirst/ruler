bb6ba46b9e9c69ac8e0707e4eeae9ef7e8ffe616
Michael <mokchira@gmail.com>
2020-03-24 21:20:53 -0400

    working, going to upgrade to full geometry drawable

--- ruler.py
+++ ruler.py
@@ -13,91 +13,144 @@ Date Created:   March 24, 2020 - 11:28:31
 import hou
 import viewerstate.utils as su
 
+def createSphereGeometry():
+    geo = hou.Geometry()
+    sphere_verb = hou.sopNodeTypeCategory().nodeVerb("sphere")
+
 class State(object):
-    MSG = "Move the mouse over the geometry."
+    msg = "Click and drag on the geometry to measure it."
+    default_font_size = 30.0
+    default_color = "green"
+    default_text = "default text"
 
     def __init__(self, state_name, scene_viewer):
         self.state_name = state_name
         self.scene_viewer = scene_viewer
-
-        # A drawable to display the mouse coordinates at the cursor 
-        # position.
-        # Use su.CursorLabel.setLabel to display a fix label.
-        self.cursor = su.CursorLabel(scene_viewer, "cursor")
-        
-        # Drawable for drawing the polygon under the cursor.
-        self.face = hou.GeometryDrawable(self.scene_viewer, 
-            hou.drawableGeometryType.Face, 
-            "face", 
-            params = {
-                "style": hou.drawableGeometryFaceStyle.Plain,
-                "color1": (0.0,1.0,0.0,1.0) }
-        )
+        self.text_drawable = hou.TextDrawable(scene_viewer, "text_drawable")
+        self.initial_spot_drawable = hou.SimpleDrawable(scene_viewer, hou.drawablePrimitive.Sphere, "initial_spot")
+        self.current_spot_drawable = hou.SimpleDrawable(scene_viewer, hou.drawablePrimitive.Sphere, "current_spot")
+        self.initial_spot_drawable.setDisplayMode(hou.drawableDisplayMode.CurrentViewportMode)
+        self.current_spot_drawable.setDisplayMode(hou.drawableDisplayMode.CurrentViewportMode)
+        self.text_params = {'text': None, 'translate' : hou.Vector3(0.0, 0.0, 0.0)}
+        self.initial_pos = hou.Vector3(0.0, 0.0, 0.0)
+        self.current_pos = hou.Vector3(0.0, 0.0, 0.0)
+        self.measurement = 0.0
+        self.geometry = None
+        self.current_node = None
+        self.font_color = State.default_color
+        self.font_size = State.default_font_size
+        self.text = State.default_text
+        self.updateTextField()
                 
     def show(self, visible):
         """ Display or hide drawables.
         """
-        self.cursor.show(visible)
-        self.face.show(visible)
+        self.text_drawable.show(visible)
+        self.initial_spot_drawable.show(visible)
+        self.current_spot_drawable.show(visible)
 
-    def onEnter(self, kwargs):
+    def enableDrawables(self, enable):
+        self.initial_spot_drawable.enable(enable)
+        self.current_spot_drawable.enable(enable)
+
+    def setText(self, text):
+        self.text = text
+        self.updateTextField()
+
+    def setFontSize(self, size):
+        self.font_size = size
+        self.updateTextField()
+
+    def setFontColor(self, color):
+        self.font_color = color
+        self.updateTextField()
+
+    def setTextPos(self, x, y):
+        self.text_params['translate'][0] = x
+        self.text_params['translate'][1] = y
+
+    def setSpotTransform(self, drawable, pos):
+        translate = hou.hmath.buildTranslate(pos)
+        viewer = hou.SceneViewer.curViewport(self.scene_viewer)
+        modelToCamera = hou.GeometryViewport.cameraToModelTransform(viewer).inverted()
+        cameraToNDC = hou.GeometryViewport.ndcToCameraTransform(viewer).inverted()
+        NDC = translate * modelToCamera * cameraToNDC
+        w = NDC.at(3, 3)
+        w *= .01
+        print w
+        scale = hou.hmath.buildScale(w, w, w)
+        transfrom = scale * translate
+        hou.SimpleDrawable.setTransform(drawable, transfrom)
+
+    def updateTextField(self):
+        font_string = '<font color="{2}" size="{1}"> {0} </font>'.format(self.text, self.font_size, self.font_color)
+        self.text_params['text'] = font_string
+
+    def getMousePos(self, ui_event):
+        device = hou.UIEvent.device(ui_event)
+        return device.mouseX(), device.mouseY()
+
+    def getIntersectionPos(self, ui_event):
+        (origin, ray) = hou.ViewerEvent.ray(ui_event)
+        (prim_num, pos, normal, uvw) = su.sopGeometryIntersection(self.geometry, origin, ray)
+        return pos
+
+    def onGenerate(self, kwargs):
         """ Assign the geometry to drawabled
         """
-        node = kwargs["node"]
-        self.geometry = node.geometry()
-        self.show(True)
-
-        self.scene_viewer.setPromptMessage( State.MSG )
+        self.measurement = 0.0;
+        self.scene_viewer.setPromptMessage( State.msg )
+        self.current_node = hou.SceneViewer.currentNode(self.scene_viewer)
+        self.geometry = hou.SopNode.geometry(self.current_node)
+        self.enableDrawables(True)
 
     def onResume(self, kwargs):
-        self.show(True)
-        self.scene_viewer.setPromptMessage( State.MSG )
+        self.scene_viewer.setPromptMessage( State.msg )
+        self.current_node = hou.SceneViewer.currentNode(self.scene_viewer)
+        self.geometry = hou.SopNode.geometry(self.current_node)
+        self.enableDrawables(True)
+
+    def onExit(self, kwargs):
+        hou.SceneViewer.clearPromptMessage(self.scene_viewer)
+        self.show(False)
+        self.enableDrawables(False)
 
     def onInterrupt(self,kwargs):
         self.show(False)
+        self.enableDrawables(False)
+
+    def onMouseActive(self, ui_event):
+        (x, y) = self.getMousePos(ui_event)
+        self.current_pos = self.getIntersectionPos(ui_event)
+        self.measurement = (self.current_pos - self.initial_pos).length()
+        self.setText(str(self.measurement))
+        self.setTextPos(x, y)
+        self.setSpotTransform(self.current_spot_drawable, self.current_pos)
+        self.show(True)
+
+    def onMouseStart(self, ui_event):
+        (x, y) = self.getMousePos(ui_event)
+        self.initial_pos = self.getIntersectionPos(ui_event)
+        self.setText("Start!")
+        self.setTextPos(x, y)
+        self.setSpotTransform(self.initial_spot_drawable, self.initial_pos)
+        self.show(True)
 
     def onMouseEvent(self, kwargs):
-        """ Computes the cursor text position and drawable geometry
-        """
-        # set the cursor label
-        self.cursor.setParams(kwargs)
-
-        # Set the drawable with the polygon under the cursor
         ui_event = kwargs["ui_event"]
-        (origin, dir) = ui_event.ray()        
-        gi = su.GeometryIntersector(self.geometry)
-        gi.intersect(origin, dir, snapping=False)
-
-        if gi.prim_num != -1 and gi.prim_num != self.poly_id:
-            self.poly_id = gi.prim_num
-    
-            # Construct a new geometry
-            poly_points = self.geometry.prim(self.poly_id).points()                                                                      
-            poly_geo = hou.Geometry()
-            poly = poly_geo.createPolygon()
-            for pt in poly_points:
-                point = poly_geo.createPoint()
-                point.setPosition(pt.position())
-                poly.addVertex(point)        
-
-            # update the drawable                
-            self.face.setGeometry(poly_geo)
-            self.show(True)
-                
-        elif gi.prim_num == -1:
-            # no polygon under the cursor
-            self.poly_id = -1
-            self.poly_geo = None            
-            self.face.show(False)
+        reason = hou.UIEvent.reason(ui_event)
+        if (reason == hou.uiEventReason.Start):
+            self.onMouseStart(ui_event)
+        if (reason == hou.uiEventReason.Active):
+            self.onMouseActive(ui_event)
+        if (reason == hou.uiEventReason.Changed):
+            self.show(False)
 
     def onDraw( self, kwargs ):
         """ This callback is used for rendering the drawables
         """
         handle = kwargs["draw_handle"]
-
-        self.face.draw(handle) 
-        self.cursor.draw(kwargs)
-
+        hou.TextDrawable.draw(self.text_drawable, handle, self.text_params)
 
 def createViewerStateTemplate():
     """ Mandatory entry point to create and return the viewer state 
@@ -110,8 +163,5 @@ def createViewerStateTemplate():
     template = hou.ViewerStateTemplate(state_typename, state_label, state_cat)
     template.bindFactory(State)
     template.bindIcon("MISC_python")
-
-
-
 
     return template
